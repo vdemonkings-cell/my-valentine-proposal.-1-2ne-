955ce18a1cdb9fc304d8bb199eca7703c80de83c
vdemonkings-cell <vdemonkings@gmail.com>
2026-02-14 11:53:54 +0530

    Create AdminDashboard component

---
+++ components/AdminDashboard.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const AdminDashboard: React.FC = () => {
+    return (
+        <div>
+            <h1>Admin Dashboard</h1>
+            <p>Welcome to the admin dashboard. Here you can manage all aspects of the application.</p>
+            {/* Additional functional components can be added here */}
+        </div>
+    );
+};
+
+export default AdminDashboard;
